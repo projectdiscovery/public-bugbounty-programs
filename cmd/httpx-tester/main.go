51ac577f6c7e41d72e500b3a6de37bf05f672a06
xm1k3 <xm1k3@protonmail.com>
2023-02-28 12:58:21 +0100

    added urls logic

--- cmd/httpx-tester/main.go
+++ cmd/httpx-tester/main.go
@@ -1,36 +1,28 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"log"
-	"os"
 
 	"github.com/projectdiscovery/goflags"
 	"github.com/projectdiscovery/httpx/runner"
-	"github.com/projectdiscovery/public-bugbounty-programs/pkg/dns"
+	fileutil "github.com/projectdiscovery/utils/file"
 )
 
-var bbListFile = flag.String("file", "../../chaos-bugbounty-list.json", "Chaos bugbounty list json file")
+var bbListFile = flag.String("file", "../../urls.txt", "Chaos bugbounty list json file")
 
 func main() {
 	flag.Parse()
 
-	rawJSON, err := os.ReadFile(*bbListFile)
+	urlFile, err := fileutil.ReadFile(*bbListFile)
 	if err != nil {
-		log.Fatalf("Failed to read initial JSON file: %v", err)
-	}
-
-	var chaosList dns.ChaosList
-	err = json.Unmarshal(rawJSON, &chaosList)
-	if err != nil {
-		log.Fatalf("Failed to parse initial JSON file: %v", err)
+		log.Fatal(err)
 	}
 
 	allUrls := goflags.StringSlice{}
 
-	for _, programs := range chaosList.Programs {
-		allUrls = append(allUrls, programs.URL)
+	for url := range urlFile {
+		allUrls = append(allUrls, url)
 	}
 
 	options := runner.Options{
