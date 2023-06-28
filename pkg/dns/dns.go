32afb340a37813314bd435cbc7c2fc547307d664
Tarun Koyalwar <tarun@projectdiscovery.io>
2023-06-28 21:13:45 +0530

    fix lint error

--- pkg/dns/dns.go
+++ pkg/dns/dns.go
@@ -1,8 +1,10 @@
 package dns
 
 import (
-	"github.com/projectdiscovery/retryabledns"
 	"strings"
+
+	"github.com/projectdiscovery/gologger"
+	"github.com/projectdiscovery/retryabledns"
 
 	"github.com/asaskevich/govalidator"
 	sliceutil "github.com/projectdiscovery/utils/slice"
@@ -10,7 +12,24 @@ import (
 	"golang.org/x/net/publicsuffix"
 )
 
-var ExcludeMap map[string]struct{}
+// DefaultResolvers contains the default list of resolvers known to be good
+
+var (
+	ExcludeMap       map[string]struct{}
+	DefaultResolvers = []string{
+		"1.1.1.1:53",        // Cloudflare primary
+		"1.0.0.1:53",        // Cloudflare secondary
+		"8.8.8.8:53",        // Google primary
+		"8.8.4.4:53",        // Google secondary
+		"9.9.9.9:53",        // Quad9 Primary
+		"9.9.9.10:53",       // Quad9 Secondary
+		"77.88.8.8:53",      // Yandex Primary
+		"77.88.8.1:53",      // Yandex Secondary
+		"208.67.222.222:53", // OpenDNS Primary
+		"208.67.220.220:53", // OpenDNS Secondary
+	}
+	client *retryabledns.Client
+)
 
 // ChaosProgram json data item struct
 type ChaosProgram struct {
@@ -31,7 +50,6 @@ func ValidateFQDN(value string) bool {
 	if err != nil {
 		// If the domain can't be parsed by publicsuffix,
 		// then we attempt to resolve a DNS A record to determine if it's valid.
-		client, err := retryabledns.New([]string{"8.8.8.8", "8.8.4.4"}, 3)
 		resp, err := client.Resolve(value)
 		if err != nil || (resp.A == nil && resp.AAAA == nil) {
 			// DNS resolution also failed, so we conclude that the domain isn't valid.
@@ -57,11 +75,18 @@ func ExtractHostname(item string) string {
 	if ValidateFQDN(trimmedStr) {
 		return trimmedStr
 	}
-
 	return ""
 }
 
 func GetUniqueDomains(first, second []string) []string {
 	_, diff := sliceutil.Diff(first, second)
 	return sliceutil.Dedupe(diff)
+}
+
+func init() {
+	var err error
+	client, err = retryabledns.New(DefaultResolvers, 3)
+	if err != nil || client == nil {
+		gologger.Fatal().Msgf("Could not create DNS client: %s\n", err)
+	}
 }
